c8e76c2080700eaea35f46595cf5ef2d54623e39
Code Master <codemaster08240328@gmail.com>
2018-10-16 01:37:39 +1000

    Engagement Page design - in progress..

--- src/pages/dashboard/Engagement.js
+++ src/pages/dashboard/Engagement.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { 
+  View, 
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity 
+} from 'react-native'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import LogoComponent from '../../components/LogoComponent'
@@ -14,6 +20,55 @@ class Engagement extends Component {
     return (
       <View style={styles.container}>
         <LogoComponent backbtn />
+        <View style={{height:60}}></View>
+        <View 
+          style={{
+            height:30, 
+            backgroundColor: colors.lightBorderColor,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <Text style={{color: colors.fontGrayColor}}>Schedule Engagement Date Form</Text>
+        </View>
+        <View style={{flex: 1}}>
+          <View style={{flex: 6}}>
+            <View style={{flex: 1, padding: 5, justifyContent: 'center', alignItems: 'center'}}>
+              <Text style={{fontSize: 20}}>Enter Engagement Info</Text>
+            </View>
+            <View style={{flex: 1, padding: 5}}>
+              <Text style = {{paddingVertical: 5}}>EngagmentDate</Text>
+              <TextInput
+                style={styles.input}
+              />
+            </View>
+            <View style={{flex: 1, padding: 5}}>
+              <Text style={{paddingVertical: 5}}>Photoshoot Timeframe</Text>
+              <TextInput
+                style={styles.input}
+              />
+            </View>
+            <View style={{flex: 1, padding: 5}}>
+              <Text style={{paddingVertical: 5}}>Engagment Venue</Text>
+              <TextInput
+                style={styles.input}
+              />
+            </View>
+            <View style={{flex: 1, padding: 5, justifyContent: 'center', alignItems: 'center'}}>
+              <TouchableOpacity 
+                style={{
+                  backgroundColor: colors.btnColor,
+                  borderRadius: 17,
+                  padding: 10
+                }}
+              >
+                <Text style={{color: colors.white}}>SUBMIT</Text>
+              </TouchableOpacity>
+            </View>
+            
+          </View>
+          <View style={{flex: 4}}></View>
+        </View>
       </View>
     )
   }
@@ -22,6 +77,13 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.white,
     flex: 1
+  },
+  input: {
+    flex: 1,
+    borderColor: colors.darkBorderColor,
+    borderRadius: 5,
+    borderWidth: 1,
+    paddingLeft: 10
   }
 })
 
